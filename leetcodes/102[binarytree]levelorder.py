0f86bbda8b6c2069bbae8120002c55a469342a66
Tejas Rane <tejas.m.rane@gmail.com>
2020-06-02 16:39:15 -0700

    add

---
+++ leetcodes/102[binarytree]levelorder.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return []
+        q = []
+        out = []
+        q.append(root)
+        while q:
+            size = len(q)
+            current_level = []
+            for _ in range(0,size):
+                ele = q.pop(0)
+                current_level.append(ele.val)
+                if ele.left:
+                    q.append(ele.left)
+                if ele.right:
+                    q.append(ele.right)
+            out.append(current_level)
+        return out
+        
